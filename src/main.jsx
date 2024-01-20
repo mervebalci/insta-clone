f1b6f5b690132ef9f446b0f8163f55d827ad1555
Merve Balci <mervebalci090@gmail.com>
2024-01-20 19:53:38 -0400

    Setting up Chakra UI and Creating the HomePage

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,39 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { ChakraProvider } from '@chakra-ui/react';
+import { extendTheme } from '@chakra-ui/react';
+import { mode } from '@chakra-ui/theme-tools';
+import { BrowserRouter } from 'react-router-dom';
+
+// To have the BLACK instagram background color
+const styles = {
+  global: (props) => ({
+    body: {
+      // mode("light mode", "dark mode")
+      bg: mode("gray.100", "#000")(props),
+      color: mode("gray.800", "whiteAplha.900")(props),
+    },
+  }),
+};
+
+// Add the color mode config
+const config = {
+  initialColorMode: 'dark',
+  useSystemColorMode: false,
+};
+
+// Extend the theme
+const theme = extendTheme({ config, styles });
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <ChakraProvider theme={theme}>
+        <App />
+      </ChakraProvider>
+    </BrowserRouter>
   </React.StrictMode>,
-)
+);
