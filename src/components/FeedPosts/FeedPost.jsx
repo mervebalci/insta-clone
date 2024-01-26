5843f57555947200ffc162ae0e8801be5c956678
Merve Balci <mervebalci090@gmail.com>
2024-01-26 12:50:46 -0400

    Designing the PostHeader

--- src/components/FeedPosts/FeedPost.jsx
+++ src/components/FeedPosts/FeedPost.jsx
@@ -1,5 +1,15 @@
+import { Box, Image } from "@chakra-ui/react";
+import PostHeader from "./PostHeader";
+import PostFooter from "./PostFooter";
+
 export default function FeedPost() {
   return (
-    <div>FeedPost</div>
+    <>
+      <PostHeader />
+      <Box>
+        <Image src="/img1.png" alt="user profile pic" />
+      </Box>
+      <PostFooter />
+    </>
   )
 }
