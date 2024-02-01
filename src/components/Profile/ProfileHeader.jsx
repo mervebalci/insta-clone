9a0e9333bb46a7a8423022d26bbe3932db611522
Merve Balci <mervebalci090@gmail.com>
2024-02-01 19:38:39 -0400

    Designing the Profile Header

--- src/components/Profile/ProfileHeader.jsx
+++ src/components/Profile/ProfileHeader.jsx
@@ -1,5 +1,64 @@
+import { Avatar, AvatarGroup, Button, Flex, Text, VStack } from "@chakra-ui/react";
+
 export default function ProfileHeader() {
   return (
-    <div>ProfileHeader</div>
+    <Flex gap={{base: 4, sm: 10}} py={10} direction={{base: "column", sm: "row"}}>
+      <AvatarGroup
+        size={{base: "xl", md: "2xl"}}
+        justifySelf={"center"}
+        alignSelf={"flex-start"}
+        mx={"auto"}
+      >
+        <Avatar name="Efes the white" src="/profilepic.png" alt="Efes' pic" />
+      </AvatarGroup>
+
+      <VStack alignItems={"start"} gap={2} mx={"auto"} flex={1}>
+        <Flex
+          gap={4}
+          direction={{base: "column", sm: "row"}}
+          justifyContent={{base: "center", sm: "flex-start"}}
+          alignItems={"center"}
+          w={"full"}
+        >
+          <Text fontSize={{base: "sm", md: "lg"}}>
+            efesthewhite_
+          </Text>
+
+          <Flex gap={4} alignItems={"center"} justifyContent={"center"}>
+            <Button
+              bg={"white"}
+              color={"black"}
+              _hover={{bg: "whiteAlpha.800"}}
+              size={{base: "xs", md: "sm"}}
+            >
+              Edit Profile
+            </Button>
+          </Flex>
+        </Flex>
+
+        <Flex alignItems={"center"} gap={{base: 2, sm: 4}}>
+          <Text fontSize={{base: "xs", md: "sm"}}>
+            <Text as="span" fontWeight={"bold"} mr={1}>4</Text>
+            Posts
+          </Text>
+
+          <Text fontSize={{base: "xs", md: "sm"}}>
+            <Text as="span" fontWeight={"bold"} mr={1}>4564</Text>
+            Followers
+          </Text>
+
+          <Text fontSize={{base: "xs", md: "sm"}}>
+            <Text as="span" fontWeight={"bold"} mr={1}>353</Text>
+            Following
+          </Text>
+        </Flex>
+
+        <Flex alignItems={"center"} gap={4}>
+          <Text fontSize={"sm"} fontWeight={"bold"}>Efes the White</Text>
+        </Flex>
+
+        <Text fontSize={"sm"}>Sleeping... Eating... Chilling... Eating again...</Text>
+      </VStack>
+    </Flex>
   )
 }
