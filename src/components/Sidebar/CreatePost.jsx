97dac43689ad77162312325ff7441160f858b6ef
Merve Balci <mervebalci090@gmail.com>
2024-03-02 20:44:30 -0400

    Searching for User Profiles

--- src/components/Sidebar/CreatePost.jsx
+++ src/components/Sidebar/CreatePost.jsx
@@ -3,26 +3,28 @@ import { CreatePostLogo } from "../../assests/constants";
 
 export default function CreatePost() {
   return (
-    <Tooltip
-      hasArrow
-      label={"Create"}
-      placement="right"
-      ml={1}
-      openDelay={500}
-      display={{base: "block", md: "none"}}
-    >
-      <Flex
-        alignItems={"center"}
-        gap={4}
-        _hover={{bg: "whiteAlpha.400"}}
-        borderRadius={6}
-        p={2}
-        w={{base: 10, md: "full"}}
-        justifyContent={{base: "center", md: "flex-start"}}
+    <>
+      <Tooltip
+        hasArrow
+        label={"Create"}
+        placement="right"
+        ml={1}
+        openDelay={500}
+        display={{base: "block", md: "none"}}
       >
-        <CreatePostLogo />
-        <Box display={{base: "none", md: "block"}}>Create</Box>
-      </Flex>
-    </Tooltip>
+        <Flex
+          alignItems={"center"}
+          gap={4}
+          _hover={{bg: "whiteAlpha.400"}}
+          borderRadius={6}
+          p={2}
+          w={{base: 10, md: "full"}}
+          justifyContent={{base: "center", md: "flex-start"}}
+        >
+          <CreatePostLogo />
+          <Box display={{base: "none", md: "block"}}>Create</Box>
+        </Flex>
+      </Tooltip>
+    </>
   )
 }
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -3,6 +3,7 @@ import { Link as RouterLink } from "react-router-dom";
 import { InstagramLogo, InstagramMobileLogo } from "../../assests/constants";
 import { BiLogOut } from "react-icons/bi";
 import useLogout from "../../hooks/useLogout";
+import SidebarItems from "./SidebarItems";
 
 export default function Sidebar() {
 
@@ -32,33 +33,7 @@ export default function Sidebar() {
         </Link>
 
         <Flex direction={"column"} gap={5} cursor={"pointer"}>
-          {sidebarItems.map((item, index) => (
-            <Tooltip 
-              key={index}
-              hasArrow
-              label={item.text}
-              placement="right"
-              ml={1}
-              openDelay={500}
-              display={{base: "block", md: "none"}}
-            >
-              <Link
-                display={"flex"}
-                to={item.link || null}
-                as={RouterLink}
-                alignItems={"center"}
-                gap={4}
-                _hover={{bg: "whiteAlpha.400"}}
-                borderRadius={6}
-                p={2}
-                w={{base: 10, md: "full"}}
-                justifyContent={{base: "center", md: "flex-start"}}
-              >
-                {item.icon}
-                <Box display={{base: "none", md: "block"}}>{item.text}</Box>
-              </Link>
-            </Tooltip>
-          ))}
+          <SidebarItems />
         </Flex>
 
         {/* LOG OUT */}
