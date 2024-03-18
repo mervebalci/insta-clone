9aef811b5c72c851b4da3d3c59c613c260019987
Merve Balci <mervebalci090@gmail.com>
2024-03-18 00:16:28 -0300

    Getting the details of the comment

--- src/components/Comment/Comment.jsx
+++ src/components/Comment/Comment.jsx
@@ -1,22 +1,46 @@
-import { Avatar, Flex, Text } from "@chakra-ui/react";
+import { Avatar, Flex, Skeleton, SkeletonCircle, Text } from "@chakra-ui/react";
+import useGetUserProfileById from "../../hooks/useGetUserProfileById";
+import { Link } from "react-router-dom";
+import { timeAgo } from "../../utils/timeAgo";
 
 export default function Comment({ comment }) {
+  // Fetching the user profile depending on the ID, which is comment.createdBy
+  const { userProfile, isLoading } = useGetUserProfileById(comment.createdBy);
+  
+  if (isLoading) return <CommentSkeleton />;
+
   return (
     <Flex gap={4}>
-      {/* <Avatar src={profilePic} name={username} size={"sm"} /> */}
+      <Link to={`/${userProfile.username}`}>
+        <Avatar src={userProfile.profilePicURL} size={"sm"} />
+      </Link>
       <Flex direction={"column"}>
-        <Flex gap={2}>
-          <Text fontWeight={"bold"} fontSize={12}>
-            {/* {username} */}
-          </Text>
+        <Flex gap={2} alignItems={"center"}>
+          <Link to={`/${userProfile.username}`}>
+            <Text fontWeight={"bold"} fontSize={12}>
+              {userProfile.username}
+            </Text>
+          </Link>
           <Text fontSize={14}>
             {comment.comment}
           </Text>
         </Flex>
         <Text fontSize={12} color={"gray"}>
-          {/* {createdAt} */}
+          {timeAgo(comment.createdAt)}
         </Text>
       </Flex>
     </Flex>
-  )
-}
+  );
+};
+
+function CommentSkeleton() {
+  return (
+    <Flex gap={4} w={"full"} alignItems={"center"}>
+      <SkeletonCircle h={10} w={10} />
+      <Flex gap={1} flexDir={"column"}>
+        <Skeleton h={2} w={100} />
+        <Skeleton h={2} w={50} />
+      </Flex>
+    </Flex>
+  );
+};
---
+++ src/hooks/useGetUserProfileById.js
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react";
+import useShowToast from "./useShowToast";
+import { doc, getDoc } from "firebase/firestore";
+import { firestore } from "../firebase/firebase";
+
+export default function useGetUserProfileById(userId) {
+  const [isLoading, setIsLoading] = useState(true);
+  // State is initially true, because we will call it within useEffect.
+
+  const [userProfile, setUserProfile] = useState(null);
+
+  const showToast = useShowToast();
+
+  useEffect(() => {
+    async function getUserProfile() {
+      setIsLoading(true);
+      setUserProfile(null);   // It is null, because it has not been fetched yet.
+
+      try {
+        const userRef = await getDoc(doc(firestore, "users", userId));
+        if (userRef.exists()) {
+          setUserProfile(userRef.data());
+        }
+
+      } catch (error) {
+        showToast("Error", error.message, "error");
+
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    getUserProfile();
+  }, [showToast, setUserProfile, userId]);   // Whenever this userId changes, it will run the useEffect to fetch the user profile.
+
+  return {isLoading, userProfile, setUserProfile};
+}
+
+// It will be called inside the comment.jsx
