48a5cc854313c0d58e0b96dcfabf67857a06c67c
Merve Balci <mervebalci090@gmail.com>
2024-04-11 17:37:36 -0300

    Security Rules and Deployment

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAH5inYpaWMi3mDkM_vkmEluoJ7GcdyJBQ",
-  authDomain: "insta-clone-67014.firebaseapp.com",
-  projectId: "insta-clone-67014",
-  storageBucket: "insta-clone-67014.appspot.com",
-  messagingSenderId: "926811193912",
-  appId: "1:926811193912:web:581b951ff7de96d0839ccd",
-  measurementId: "G-05XN2N1PR4"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
